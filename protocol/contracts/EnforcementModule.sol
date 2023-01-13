5fdf866b59dc017c2952a1982c463b549c921a33
jaxernst <104089077+jackson-clerkenwell-labs@users.noreply.github.com>
2023-01-12 17:54:24 -0800

    Enforcement module template

--- protocol/contracts/EnforcementModule.sol
+++ protocol/contracts/EnforcementModule.sol
@@ -2,11 +2,13 @@
 pragma solidity ^0.8.9;
 
 import "./BaseCommitment.sol";
+import "./commitment-types/scheduled/DeadlineCommitment.sol";
 
+/**
+ * Template that enforcement modules must implement. 
+ */
 abstract contract EnforcementModule {
-    uint entries;
-
-    //function join(address commitment, bytes memory joinData) public payable virtual;
-    //function exit() public virtual;
-    //function penalize(address commitment) public virtual;
+    function join(DeadlineCommitment commitment, bytes calldata joinData) public payable virtual;
+    function penalize(address user) public virtual;
+    function exit() public virtual;
 }
