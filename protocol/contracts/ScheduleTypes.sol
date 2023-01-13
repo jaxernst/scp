e2bc6e039d01b7831a965e171f64621f9dfd91b2
jaxernst <104089077+jackson-clerkenwell-labs@users.noreply.github.com>
2023-01-12 17:54:38 -0800

    Comment update

--- protocol/contracts/ScheduleTypes.sol
+++ protocol/contracts/ScheduleTypes.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+/**
+ * Types of schedules that commitments can utilize and track progress with
+ */
 enum ScheduleType {
     NONE,
     DEADLINE,
