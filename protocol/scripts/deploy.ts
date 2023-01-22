6f734af6fff47b7e93ada876ddf4d457cecc996b
jaxernst <104089077+jackson-clerkenwell-labs@users.noreply.github.com>
2023-01-22 09:19:14 -0800

    Protocol deployment script update

--- protocol/scripts/deploy.ts
+++ protocol/scripts/deploy.ts
@@ -5,12 +5,6 @@ async function main() {
   const factory = await ethers.getContractFactory("CommitmentHub")
   const CPH = await factory.deploy()
   console.log("Deployed protocol hub to", CPH.address)
-
-  const [signer,] = await ethers.getSigners()
-  signer.sendTransaction({
-    to: "0xdD044684cfA651c491b844AE0E8646aD7c56205b",
-    value: parseEther('1')
-  })
 }
 
 main().catch((error) => {
