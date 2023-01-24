b41a0d2279f057dd05b012143957ef947bab56d1
jaxernst <104089077+jackson-clerkenwell-labs@users.noreply.github.com>
2023-01-24 10:12:48 -0800

    Fix commitment hub retrieval

--- interface/src/lib/getContract.ts
+++ interface/src/lib/getContract.ts
@@ -1,18 +1,16 @@
-import type { AlarmPool, CommitmentProtocolHub } from "@social-alarm-clock/protocol/typechain-types";
-import alarmPool from "@social-alarm-clock/protocol/artifacts/contracts/AlarmPool.sol/AlarmPool.json"
-
+import type { CommitmentHub } from "@scp/protocol/typechain-types"
 import { ethers, type Contract } from "ethers";
 import { contracts } from "svelte-ethers-store"
 import { get } from "svelte/store";
 
 
-export function getComittmentProtocolHub(): CommitmentProtocolHub | undefined {
+export function getComittmentHub(): CommitmentHub | undefined {
     const contract = get(contracts)["CommitmentProtocolHub"]
     if (!contract) return
-    return contract as unknown as CommitmentProtocolHub
+    return contract as unknown as CommitmentHub
 }
 
-export async function getPool(hub: CommitmentProtocolHub, poolId: number): Promise<AlarmPool> {
+export async function getCommitmnet(hub: CommitmentHub, poolId: number): Promise<Commitment> {
     const addr = await hub.deployedPools(poolId)
     if (!addr) throw Error('Pool not found')
 
