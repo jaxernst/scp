e8c409c9c56566a42d2c19496c9693c182ff680c
jaxernst <104089077+jackson-clerkenwell-labs@users.noreply.github.com>
2023-01-24 10:12:12 -0800

    Testing forms submission

--- interface/src/components/new-commitment/NewTimelockingDeadlineTask.svelte
+++ interface/src/components/new-commitment/NewTimelockingDeadlineTask.svelte
@@ -1,6 +1,12 @@
 <script lang=ts>
 	import { BigNumber } from "ethers";
+  import { getComittmentHub } from "$lib/getContract";
 
+  const onSubmit = async () => {
+    const hub = getComittmentHub()
+    if (!hub) throw "No Hub"
+
+  }
   let taskDescription = "task description"
   let deadline: number
   let stakeAmount: BigNumber = BigNumber.from(0)
@@ -10,6 +16,7 @@
 <input type=text bind:value={taskDescription}>
 <input type=radio bind:value={taskDescription}>
 <input type=check bind:value={taskDescription}>
+<button on:click={onSubmit}>Submit</button>
 
 <style>
   input {
@@ -19,7 +26,6 @@
     outline: none;
     background-color: var(--theme-color3);
     border-radius: 14px;
-
   }
 
 
