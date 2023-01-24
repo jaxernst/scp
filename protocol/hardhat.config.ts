8bc7615733b1760f04a69704c816ceb3afe29824
jaxernst <104089077+jackson-clerkenwell-labs@users.noreply.github.com>
2023-01-24 10:12:58 -0800

    Auto push ABIs to interface folder

--- protocol/hardhat.config.ts
+++ protocol/hardhat.config.ts
@@ -1,10 +1,24 @@
 import { HardhatUserConfig } from "hardhat/config";
 import '@nomiclabs/hardhat-ethers';
 import '@nomicfoundation/hardhat-chai-matchers';
+import 'hardhat-abi-exporter'
 import "@typechain/hardhat";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  abiExporter: [
+    {
+      runOnCompile: true,
+      path: '../interface/src/abi',
+      format: "json",
+      flat: true,
+      only: [
+        "CommitmentHub.sol",
+        "TimelockingDeadlineTask.sol",
+        "BaseCommitment.sol"
+      ]
+    },
+  ]
 };
 
 export default config;
