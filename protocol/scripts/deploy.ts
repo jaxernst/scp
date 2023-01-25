89c2337208b99c0a1b719310bf2a01faff04debe
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-01-25 10:58:32 -0800

    Register commitments when dpeloying hub

--- protocol/scripts/deploy.ts
+++ protocol/scripts/deploy.ts
@@ -1,10 +1,29 @@
 import { parseEther } from "ethers/lib/utils";
 import { ethers } from "hardhat";
+import { createCommitment } from "../lib/commitmentCreation";
+import { CommitmentType, commitmentTypeVals } from "../lib/types";
+import { CommitmentHub } from "../typechain-types";
 
+/** 
+ * Deploy protocol hub and register commitment types
+ */
 async function main() {
   const factory = await ethers.getContractFactory("CommitmentHub")
   const CPH = await factory.deploy()
   console.log("Deployed protocol hub to", CPH.address)
+
+  await registerType(CPH, "BaseCommitment")
+  console.log("Registered BaseCommitment")
+  await registerType(CPH, "TimelockingDeadlineTask")
+  console.log("Registered TimelockingDeadlineCommitment")
+}
+
+async function registerType(hub: CommitmentHub, type: CommitmentType) {
+  const contract = await (await ethers.getContractFactory(type)).deploy()
+  await (await (hub.registerCommitType(
+    commitmentTypeVals[type], 
+    contract.address
+  ))).wait()
 }
 
 main().catch((error) => {
