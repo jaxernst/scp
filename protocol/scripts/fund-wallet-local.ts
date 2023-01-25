fb30df604926660262c8e86c3264c1b4e0c9562b
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-01-25 10:58:20 -0800

    Limit to only localhost

--- protocol/scripts/fund-wallet-local.ts
+++ protocol/scripts/fund-wallet-local.ts
@@ -1,11 +1,13 @@
 import { parseEther } from "ethers/lib/utils";
-import { ethers } from "hardhat";
+import hre, { ethers } from "hardhat";
 import * as dotenv from 'dotenv'
 dotenv.config()
 
 const VAL = parseEther('1')
 
 async function main() {
+  if (hre.network.name !== "localhost") throw Error(`Localhost not selected`)
+
   if (!process.env.TEST_WALLET) {
     throw Error(".env must have TEST_WALLET to send funds to")
   }
