c715fbc709d5cf57bbd9f8aad43b815b84d0d448
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-02-28 21:00:24 -0800

    Add leter toggle

---
+++ dapps/social-alarm-clock/src/ToggleLetter.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  export let value: string = ''
+  export let toggled = false
+</script>
+
+<button style="all:unset" on:click={() => toggled = !toggled}>
+  <div class={"letter" + (toggled ? "active" : "")}>
+    {value}
+  </div>
+</button>
+
+<style>
+  .letter {
+    color: rgb(133, 133, 133);
+    font-weight: 400;
+    cursor: pointer;
+  }
+
+  .letter:hover {
+    text-shadow: 1px 1px 2px black;
+  }
+
+  .active {
+    color: white;
+    font-weight: 800;
+  }
+</style>
