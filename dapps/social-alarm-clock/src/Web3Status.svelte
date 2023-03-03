772681e0275f1218702cf59571c58c1fa8c7ff7c
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-03-02 19:36:01 -0800

    Formatting

--- dapps/social-alarm-clock/src/Web3Status.svelte
+++ dapps/social-alarm-clock/src/Web3Status.svelte
@@ -1,20 +1,20 @@
-<script lang=ts>
-  import { account, ensName } from "./chainClient"
+<script lang ts>
+  import { account, ensName } from "./chainClient";
   import { web3Modal } from "./chainClient";
-  import { shorthandAddress } from "@scp/dapp-lib"
+  import { shorthandAddress } from "@scp/dapp-lib";
 
-  let displayName
+  let displayName;
   $: if ($account) {
-    displayName = $ensName ? $ensName : shorthandAddress($account.address)
+    displayName = $ensName ? $ensName : shorthandAddress($account.address);
   }
 
-  $: indicatorColor = $account && $account.isConnected ? "green" : "red"
+  $: indicatorColor = $account && $account.isConnected ? "green" : "red";
 </script>
 
 {#if $account && displayName}
-  <div class=container on:click={$web3Modal.openModal}>
-    <div class=displayName>{displayName}</div>
-    <div class="indicator" style="background-color:{indicatorColor}"/>
+  <div class="container" on:click={$web3Modal.openModal}>
+    <div class="displayName">{displayName}</div>
+    <div class="indicator" style="background-color:{indicatorColor}" />
   </div>
 {/if}
 
@@ -22,19 +22,19 @@
   .container {
     display: flex;
     justify-content: flex-end;
-    background: none
+    background: none;
   }
 
   .indicator {
-        height: 10px;
-        width: 10px;
-        border-radius: 100%;
-        background-color: var(--indicator-color);
-        margin: 0 .5em;
-    }
+    height: 10px;
+    width: 10px;
+    border-radius: 100%;
+    background-color: var(--indicator-color);
+    margin: 0 0.5em;
+  }
 
-    .displayName {
-      font-size: smaller;
-      color: rgb(201, 145, 34);
-    }
+  .displayName {
+    font-size: smaller;
+    color: rgb(201, 145, 34);
+  }
 </style>
