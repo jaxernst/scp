6bda95335f8ceba5e2966b6ae9c8dcc9d9d3f58d
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-03-04 19:31:07 -0800

    Remove selection wheel

--- dapps/social-alarm-clock/src/gameState.ts
+++ dapps/social-alarm-clock/src/gameState.ts
@@ -1,4 +1,4 @@
-import { writable } from "svelte/store";
+import { derived, get, writable } from "svelte/store";
 
 enum View {
   CONNECT_WALLET,
@@ -8,4 +8,26 @@ enum View {
   ALARM_ACTIVE,
 }
 
+const SelectionWheel = (items: number) => {
+  const selected = writable(0);
+
+  return {
+    subscribe: derived(selected, (s) => ({
+      selected: s,
+      atEnd: s === items - 1,
+      atStart: s === 0,
+    })).subscribe,
+
+    next: () => {
+      if (get(selected) === items - 1) return console.log("Return");
+      selected.set(get(selected) + 1);
+    },
+    prev: () => {
+      if (get(selected) === 0) return console.log("return");
+      selected.set(get(selected) - 1);
+    },
+  };
+};
+
+export const createGameOptions = SelectionWheel(4);
 export const gameActive = writable(false);
