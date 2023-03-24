0d2d2ea486f9318b7173b6bdef8fd987832d3e14
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-03-24 16:45:43 -0700

    Get alarm creation working

--- dapps/social-alarm-clock/src/create-new-alarm/CreateNewAlarm.svelte
+++ dapps/social-alarm-clock/src/create-new-alarm/CreateNewAlarm.svelte
@@ -1,23 +1,71 @@
 <script lang="ts">
   import { prepareWriteContract, writeContract } from "@wagmi/core";
   import CommitmentHubAbi from "@scp/sdk/abi/CommitmentHub.json";
-  import { alarmDays, buyIn, isReady, SelectionWheel } from "./alarmCreation";
+  import { encodeCreationParams } from "@scp/sdk/src/scp-helpers";
+  import {
+    alarmDays,
+    alarmTime,
+    buyIn,
+    isReady,
+    missedAlarmPenalty,
+    otherPlayer,
+    SelectionWheel,
+    submissionWindow,
+  } from "./alarmCreation";
   import SelectBuyIn from "./form/SelectBuyIn.svelte";
   import SelectDays from "./form/SelectDays.svelte";
   import SelectTime from "./form/SelectTime.svelte";
   import SelectTimezoneMode from "./form/SelectTimezoneMode.svelte";
+  import { parseEther } from "ethers/lib/utils.js";
+  import { get } from "svelte/store";
 
   const numSelections = 5;
   const selections = SelectionWheel(5);
   $: selected = $selections.selected;
 
+  const dayValueMap = { Su: 1, M: 2, T: 3, W: 4, Th: 5, F: 6, Sa: 7 };
   const createAlarm = async () => {
+    const alarmDaysArr = Object.entries($alarmDays).reduce(
+      (acc, [day, selected]) => {
+        if (selected) {
+          acc.push(dayValueMap[day]);
+        }
+        return acc;
+      },
+      [] as number[]
+    );
+
+    // We have '$alarmTIme' as as string in the format of 'HH:MM', and we must convert that
+    // to a time of day in seconds
+    const [hours, minutes] = $alarmTime.split(":");
+    const alarmTimeSeconds = parseInt(hours) * 60 * 60 + parseInt(minutes) * 60;
+    console.log(
+      alarmTimeSeconds,
+      alarmDaysArr,
+      $missedAlarmPenalty,
+      $submissionWindow,
+
+      $otherPlayer
+    );
+    const encodedParams = encodeCreationParams("PartnerAlarmClock", {
+      alarmTime: alarmTimeSeconds,
+      alarmdays: alarmDaysArr,
+      missedAlarmPenalty: $missedAlarmPenalty,
+      submissionWindow: $submissionWindow,
+      timezoneOffset: -7 * 12,
+      otherPlayer: $otherPlayer,
+    });
+
     const config = await prepareWriteContract({
       address: "0x5fbdb2315678afecb367f032d93f642f64180aa3",
       abi: CommitmentHubAbi,
       functionName: "createCommitment",
-      args: [0, "0x"],
+      args: [2, encodedParams],
+      overrides: {
+        value: parseEther(get(buyIn).toString()),
+      },
     });
+
     const data = await writeContract(config);
   };
   const formComponents = [
