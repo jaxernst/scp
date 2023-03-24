0c13af5214f032a6f85d4a423858a73b68530be9
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-03-23 19:43:50 -0700

    Add time helpers for testing alarm schedule

--- protocol/test/helpers/time.ts
+++ protocol/test/helpers/time.ts
@@ -1,18 +1,24 @@
-import { BigNumber } from 'ethers';
-import { network } from 'hardhat';
+import { BigNumber } from "ethers";
+import { network } from "hardhat";
 
-import { bn } from './numbers';
+import { bn } from "./numbers";
 
 export function systemTimestamp(): number {
   return Math.floor(Date.now() / 1000);
 }
 
-export function getTimestampAtTime(time: string, date = "January 1, 2020"): number {
+export function getTimestampAtTime(
+  time: string,
+  date = "January 1, 2020"
+): number {
   return new Date(date + " " + time).getTime() / 1000;
 }
 
 export const currentTimestamp = async (): Promise<BigNumber> => {
-  const { timestamp } = await network.provider.send('eth_getBlockByNumber', ['latest', true]);
+  const { timestamp } = await network.provider.send("eth_getBlockByNumber", [
+    "latest",
+    true,
+  ]);
   return bn(timestamp);
 };
 
@@ -21,7 +27,21 @@ export const fromNow = async (seconds: number): Promise<BigNumber> => {
   return now.add(seconds);
 };
 
-export const lastBlockNumber = async (): Promise<number> => Number(await network.provider.send('eth_blockNumber'));
+export const lastBlockNumber = async (): Promise<number> =>
+  Number(await network.provider.send("eth_blockNumber"));
+
+// Get time of day in seconds from a utc timestamp
+export const timeOfDay = (timestamp: number): number => {
+  const date = new Date(timestamp * 1000);
+  return (
+    date.getUTCHours() * 3600 + date.getUTCMinutes() * 60 + date.getUTCSeconds()
+  );
+};
+
+export const dayOfWeek = (timestamp: number): number => {
+  const date = new Date(timestamp * 1000);
+  return date.getUTCDay() + 1;
+};
 
 export const SECOND = 1;
 export const MINUTE = SECOND * 60;
