376bce9986ceab321aed0a8a90d5b2a268731950
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-03-27 19:21:24 -0700

    Add function param

--- sdk/src/scp-helpers.ts
+++ sdk/src/scp-helpers.ts
@@ -1,98 +1,108 @@
 import {
-	commitmentFactories,
-	commitmentTypeVals,
-	commitmentValToType,
-	CommitStatus,
-	solidityInitializationTypes,
-	type CommitmentContractTypes,
-	type CommitmentType,
-	type InitializationTypes
-} from '@scp/protocol/lib/types';
-import type { CommitmentHub } from '@scp/protocol/typechain-types';
-import { BaseCommitment, CommitmentInitializedEvent } from '@scp/protocol/typechain-types/contracts/BaseCommitment';
-import type { CommitmentCreationEvent } from '@scp/protocol/typechain-types/contracts/CommitmentHub.sol/CommitmentHub';
-import { ethers, Signer, type BigNumberish } from 'ethers';
-
+  commitmentFactories,
+  commitmentTypeVals,
+  commitmentValToType,
+  CommitStatus,
+  solidityInitializationTypes,
+  type CommitmentContractTypes,
+  type CommitmentType,
+  type InitializationTypes,
+} from "@scp/protocol/lib/types";
+import type { CommitmentHub } from "@scp/protocol/typechain-types";
+import {
+  BaseCommitment,
+  CommitmentInitializedEvent,
+} from "@scp/protocol/typechain-types/contracts/BaseCommitment";
+import type { CommitmentCreationEvent } from "@scp/protocol/typechain-types/contracts/CommitmentHub.sol/CommitmentHub";
+import { ethers, Signer, type BigNumberish } from "ethers";
 
 export async function createCommitment<T extends CommitmentType>(
-	hub: CommitmentHub,
-	type: T,
-	initData: InitializationTypes[T],
-	value?: BigNumberish
+  hub: CommitmentHub,
+  type: T,
+  initData: InitializationTypes[T],
+  value?: BigNumberish
 ) {
-	const byteData = encodeCreationParams(type, initData);
-	return hub.createCommitment(commitmentTypeVals[type], byteData, {
-		value: value ? value : 0
-	});
+  const byteData = encodeCreationParams(type, initData);
+  return hub.createCommitment(commitmentTypeVals[type], byteData, {
+    value: value ? value : 0,
+  });
 }
 
 export function encodeCreationParams<T extends CommitmentType>(
-	name: T,
-	initData: InitializationTypes[T]
+  name: T,
+  initData: InitializationTypes[T]
 ): string {
-	return ethers.utils.defaultAbiCoder.encode(
-		solidityInitializationTypes[name],
-		Object.values(initData)
-	);
+  return ethers.utils.defaultAbiCoder.encode(
+    solidityInitializationTypes[name],
+    Object.values(initData)
+  );
 }
 
 export function getCommitment<T extends keyof CommitmentContractTypes>(
-	type: T,
-	address: string,
-	signer: Signer
+  type: T,
+  address: string,
+  signer: Signer
 ): CommitmentContractTypes[T] {
-	const factory = commitmentFactories[type];
-	return factory.connect(address, signer) as CommitmentContractTypes[T];
+  const factory = commitmentFactories[type];
+  return factory.connect(address, signer) as CommitmentContractTypes[T];
 }
 
 export interface UserCommitment {
-	contract: CommitmentContractTypes[CommitmentType];
-	creationBlock: number;
-	description: string;
-	status: CommitStatus;
+  contract: CommitmentContractTypes[CommitmentType];
+  creationBlock: number;
+  description: string;
+  status: CommitStatus;
 }
 
 export async function getUserCommitments(
-	hub?: CommitmentHub,
-	user?: Signer
+  hub?: CommitmentHub,
+  user?: Signer
 ): Promise<Record<number, UserCommitment> | undefined> {
-	if (!hub || !user) return
+  if (!hub || !user) return;
 
-	const creationEvents = await queryCommitmentCreationEvents(
-		hub, 
-		await user.getAddress()
-	);
+  const creationEvents = await queryCommitmentCreationEvents(
+    hub,
+    await user.getAddress()
+  );
 
-	if (!creationEvents) return
+  if (!creationEvents) return;
 
-	const out: Record<number, UserCommitment> = {}
-	const queryResults = []
-	for (const { args, blockNumber } of creationEvents) {
-		const contract = getCommitment(
-			commitmentValToType[args._type], 
-			args.commitmentAddr, 
-			user
-		);
+  const out: Record<number, UserCommitment> = {};
+  const queryResults = [];
+  for (const { args, blockNumber } of creationEvents) {
+    const contract = getCommitment(
+      commitmentValToType[args._type],
+      args.commitmentAddr,
+      user
+    );
 
-		out[args.id.toNumber()] = {
-			contract,
-			creationBlock: blockNumber,
-			description: await getDescription(contract),
-			status: await contract.status(),
-		}
+    out[args.id.toNumber()] = {
+      contract,
+      creationBlock: blockNumber,
+      description: await getDescription(contract),
+      status: await contract.status(),
+    };
+  }
 
-	}
-
-	return out;
+  return out;
 }
 
 export async function getDescription(commit: BaseCommitment) {
-	const initEvent = await commit.queryFilter(
-		commit.filters.CommitmentInitialized(),
-	)
-	return initEvent[0].args.description
+  const initEvent = await commit.queryFilter(
+    commit.filters.CommitmentInitialized()
+  );
+  return initEvent[0].args.description;
 }
 
-export async function queryCommitmentCreationEvents(hub: CommitmentHub, address: string) {
-	return await hub?.queryFilter(hub.filters.CommitmentCreation(address));
+export async function queryCommitmentCreationEvents(
+  hub: CommitmentHub,
+  address: string,
+  commitmentType?: CommitmentType
+) {
+  return await hub.queryFilter(
+    hub.filters.CommitmentCreation(
+      address,
+      commitmentType ? commitmentTypeVals[commitmentType] : undefined
+    )
+  );
 }
