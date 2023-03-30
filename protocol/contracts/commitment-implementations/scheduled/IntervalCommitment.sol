c8faf1bdbaef9799dc4bc36456e5ac2b6b78d33a
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-03-30 14:41:00 -0700

    More renaming and reorganizing

--- protocol/contracts/commitment-implementations/scheduled/IntervalCommitment.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
-
-/**
- * Not yet implemented
- * An interval commitment requires confirmations to be submitted at a constant time interval
- * (i.e. every day, every month, every hour, etc.)
- */
