cc1edf727fa90c8b983dad313b4d1d72275ef5c3
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-03-29 22:44:59 -0700

    Refactor to setup for SubHubs

--- protocol/contracts/CommitmentHub.sol
+++ protocol/contracts/CommitmentHub.sol
@@ -12,23 +12,20 @@ import "hardhat/console.sol";
  * @notice The commitment factory contains a registry of Commitment implementations,
  * or commitment templates. When creating new commitment implementations, they must be
  * audited and approved, so the registration process is currently permissioned.
- * 
+ *
  * ToDo: Manage commitment registration through governance
  */
 contract CommitmentFactory is Ownable {
     mapping(RegisteredCommitmentType => address) public commitmentRegistry;
 
-    function _createCommitment(RegisteredCommitmentType _type)
-        internal
-        returns (BaseCommitment)
-    {
-        require(
-            commitmentRegistry[_type] != address(0),
-            "TYPE_NOT_REGISTERED"
-        );
+    function _createCommitment(
+        RegisteredCommitmentType _type
+    ) internal returns (BaseCommitment) {
+        require(commitmentRegistry[_type] != address(0), "TYPE_NOT_REGISTERED");
         return BaseCommitment(Clones.clone(commitmentRegistry[_type]));
     }
 
+    // Commit types are standalone commitments that do not require aggreation or management
     function registerCommitType(
         RegisteredCommitmentType _type,
         address deployedAt
@@ -36,19 +33,24 @@ contract CommitmentFactory is Ownable {
         require(commitmentRegistry[_type] == address(0), "TYPE_REGISTERED");
         commitmentRegistry[_type] = deployedAt;
     }
+
+    // Sub hubs are commitment deployers with specialized functionality to manage the commitments
+    // deployed under them.
+    function registerSubHub() onlyOwner {}
 }
 
 /**
  * @notice Manage the process of creating new commitmenets and tracking currently deployed commitments.
  * The stores deployed commitments and contains events for frontends to index and track
  * user's commitments
- * 
+ *
  * Commitments and Commitment pools deployed from the hub are deployed as Minimal Proxies (clones), to reduce gas
  * costs.
  */
-contract CommitmentHub is CommitmentFactory {
+contract ScpHub is CommitmentFactory {
     uint public nextCommitmentId = 1;
     mapping(uint => BaseCommitment) public commitments;
+    mapping(uint => CommitmentHub) public hubs;
 
     event CommitmentCreation(
         address indexed user,
@@ -56,6 +58,11 @@ contract CommitmentHub is CommitmentFactory {
         address commitmentAddr,
         uint id
     );
+
+    event HubCreation(
+        address indexed creator
+        address indexed hubAddr
+    )
 
     /**
      * Creates and initializes a commitment
@@ -65,8 +72,8 @@ contract CommitmentHub is CommitmentFactory {
         bytes memory _initData
     ) public payable {
         BaseCommitment commitment = _createCommitment(_type);
-        commitment.init{ value: msg.value }(_initData);
-        
+        commitment.init{value: msg.value}(_initData);
+
         uint id = ++nextCommitmentId;
         commitments[id] = commitment;
         emit CommitmentCreation(msg.sender, _type, address(commitment), id);
