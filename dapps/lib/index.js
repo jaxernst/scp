acf8dd5d6725d2e5a2f66a9f293b75a56e859908
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-03-31 18:08:28 -0700

    Export utils

--- dapps/lib/index.js
+++ dapps/lib/index.js
@@ -1,10 +1,11 @@
-import EthSymbol from "./components/EthSymbol.svelte"
-import ClockDisplay from "./components/ClockDisplay.svelte"
-import Web3Modal from "./components/Web3Modal.svelte"
+import EthSymbol from "./components/EthSymbol.svelte";
+import ClockDisplay from "./components/ClockDisplay.svelte";
+import Web3Modal from "./components/Web3Modal.svelte";
 
-import { shorthandAddress } from "./util"
+import { shorthandAddress, timeString } from "./util";
 
-export { EthSymbol }
-export { ClockDisplay }
-export { Web3Modal }
-export { shorthandAddress }
+export { EthSymbol };
+export { ClockDisplay };
+export { Web3Modal };
+export { shorthandAddress };
+export { timeString };
