54e04450de9af49578eb166ed0c777c3c30353d3
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-03-31 18:08:22 -0700

    Add dapp lib utils

--- dapps/lib/util.ts
+++ dapps/lib/util.ts
@@ -1,3 +1,14 @@
 export const shorthandAddress = (address: string) => {
-  return "0x" + address.slice(2, 4) + "..." + address.slice(-4)
+  return "0x" + address.slice(2, 4) + "..." + address.slice(-4);
+};
+
+export function timeString(secondsAfterMidnight: number) {
+  var hours = Math.floor(secondsAfterMidnight / 3600);
+  var minutes = Math.floor((secondsAfterMidnight % 3600) / 60);
+  var ampm = hours >= 12 ? "PM" : "AM";
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  var minutesFormatted = minutes < 10 ? "0" + minutes : minutes;
+  var strTime = hours + ":" + minutesFormatted + " " + ampm;
+  return strTime;
 }
