5e318a6e8f1d53b79bfe36962aedfe25c31e1738
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-03-31 18:08:38 -0700

    Refactor and format

--- dapps/lib/components/ClockDisplay.svelte
+++ dapps/lib/components/ClockDisplay.svelte
@@ -1,78 +1,85 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	export let overrideTime: string | null = null
-	export let fontSize: string = "5rem"
+  import { onMount } from "svelte";
+  export let overrideTime: string | null = null;
+  export let overrideColor: string | null = "";
 
-	let date = new Date();
-	
-	$: time = overrideTime ? overrideTime :
-		date.toLocaleTimeString()[1] === ":" ? 
-		"0" + date.toLocaleTimeString() :
-		date.toLocaleTimeString();
+  let date = new Date();
 
-	onMount(() => {
-		const timerId = setInterval(() => {
-			date = new Date();
-		}, 1000);
+  $: time = overrideTime
+    ? overrideTime
+    : date.toLocaleTimeString()[1] === ":"
+    ? "0" + date.toLocaleTimeString()
+    : date.toLocaleTimeString();
 
-		return () => {
-			clearInterval(timerId);
-		};
-	});
+  onMount(() => {
+    const timerId = setInterval(() => {
+      date = new Date();
+    }, 1000);
 
-	const backgroundText = () => {
-		let background = ""
-		for (let i=0; i<time.length; i++) {
-			if (["A", "M", "P"].includes(time[i])) {
-				background += "8"
-			} else if (time[i] === " " || isNaN(Number(time[i]))) {
-				background += time[i]
-			} else {
-				background += "8"
-			}
-		}
-		return background
-	}
+    return () => {
+      clearInterval(timerId);
+    };
+  });
 
+  const backgroundText = () => {
+    let background = "";
+    for (let i = 0; i < time.length; i++) {
+      if (["A", "M", "P"].includes(time[i])) {
+        background += "8";
+      } else if (time[i] === " " || isNaN(Number(time[i]))) {
+        background += time[i];
+      } else {
+        background += "8";
+      }
+    }
+    return background;
+  };
 </script>
 
-<div class="clock-text-container" style="--font-size: {fontSize}">
-	<div class="clock-text">
-		{time}
-	</div>
-	<div class="clock-text background"> 
-		{backgroundText()}
-	</div>
+<div
+  class="clock-text-container"
+  style={overrideColor ? `color: ${overrideColor}` : ""}
+>
+  <div class="clock-text">
+    {time}
+  </div>
+  <div class="clock-text background">
+    {backgroundText()}
+  </div>
 </div>
 
 <style>
-	/* latin */
-	@font-face {
-		font-family: 'digital-clock';
-		src: url('fonts/digital-7.ttf');
-	}
+  /* latin */
+  @font-face {
+    font-family: "digital-clock";
+    src: url("fonts/digital-7.ttf");
+  }
 
-	.clock-text-container {
-		position: relative;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		text-align: center;
-		overflow: hidden;
-		white-space: nowrap;
-	}
-	
-	.clock-text {
-		align-items: center;
-		font-size: var(--font-size);
-		font-family: "digital-clock", sans-serif;
-		font-weight: 400;
-		color: white;
-	}
+  .clock-text-container {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    overflow: hidden;
+    white-space: nowrap;
+    margin: 0;
+    padding: 0;
+  }
 
-	.background {
-		opacity: .05;
-		position: absolute;
-		text-shadow: 5px 3px #000000;
-	}
+  .clock-text {
+    align-items: center;
+    font-size: 1em;
+    font-family: "digital-clock", sans-serif;
+    font-weight: 500;
+
+    margin: 0;
+    padding: 0;
+  }
+
+  .background {
+    opacity: 0.05;
+    position: absolute;
+    text-shadow: 5px 3px #000000;
+  }
 </style>
