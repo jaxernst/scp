d7af7d032e2ed6a510e0edc08bcd75ec2e4578fd
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-04-14 17:46:22 -0700

    Add time mformat function

--- dapps/lib/util.ts
+++ dapps/lib/util.ts
@@ -12,3 +12,42 @@ export function timeString(secondsAfterMidnight: number) {
   var strTime = hours + ":" + minutesFormatted + " " + ampm;
   return strTime;
 }
+
+export function formatTime(timeInSeconds: number) {
+  const MINUTE_IN_SECONDS = 60;
+  const HOUR_IN_SECONDS = MINUTE_IN_SECONDS * 60;
+  const DAY_IN_SECONDS = HOUR_IN_SECONDS * 24;
+  const WEEK_IN_SECONDS = DAY_IN_SECONDS * 7;
+
+  if (timeInSeconds < MINUTE_IN_SECONDS) {
+    return `${timeInSeconds} second${timeInSeconds === 1 ? "" : "s"}`;
+  } else if (timeInSeconds < HOUR_IN_SECONDS) {
+    const minutes = Math.floor(timeInSeconds / MINUTE_IN_SECONDS);
+    const seconds = timeInSeconds % MINUTE_IN_SECONDS;
+    return `${minutes} minute${minutes === 1 ? "" : "s"} and ${seconds} second${
+      seconds === 1 ? "" : "s"
+    }`;
+  } else if (timeInSeconds < DAY_IN_SECONDS) {
+    const hours = Math.floor(timeInSeconds / HOUR_IN_SECONDS);
+    const minutes = Math.floor(
+      (timeInSeconds % HOUR_IN_SECONDS) / MINUTE_IN_SECONDS
+    );
+    return `${hours} hour${hours === 1 ? "" : "s"}, ${minutes} minute${
+      minutes === 1 ? "" : "s"
+    }`;
+  } else if (timeInSeconds < WEEK_IN_SECONDS) {
+    const days = Math.floor(timeInSeconds / DAY_IN_SECONDS);
+    const hours = Math.floor(
+      (timeInSeconds % DAY_IN_SECONDS) / HOUR_IN_SECONDS
+    );
+    return `${days} day${days === 1 ? "" : "s"}, ${hours} hour${
+      hours === 1 ? "" : "s"
+    }`;
+  } else {
+    const weeks = Math.floor(timeInSeconds / WEEK_IN_SECONDS);
+    const days = Math.floor((timeInSeconds % WEEK_IN_SECONDS) / DAY_IN_SECONDS);
+    return `${weeks} week${weeks === 1 ? "" : "s"}, ${days} day${
+      days === 1 ? "" : "s"
+    }`;
+  }
+}
