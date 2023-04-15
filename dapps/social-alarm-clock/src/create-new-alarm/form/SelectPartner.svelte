cdb40d3e1973b5e368fb5d128644765e3f8e4490
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-04-14 17:47:32 -0700

    Prevent self selection

--- dapps/social-alarm-clock/src/create-new-alarm/form/SelectPartner.svelte
+++ dapps/social-alarm-clock/src/create-new-alarm/form/SelectPartner.svelte
@@ -1,6 +1,12 @@
 <script>
+  import { account } from "../../lib/chainClient";
   import { otherPlayer } from "../alarmCreation";
+
+  $: error = $otherPlayer === $account?.address;
 </script>
 
 <div>Select Partner (Address)</div>
 <input id="select-partner" bind:value={$otherPlayer} />
+{#if error}
+  <div style="color: red"><i>You cannot select yourself as a partner</i></div>
+{/if}
