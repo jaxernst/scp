a392781a66c8ad0114e5aa1ccd227b6dc5bf6ae9
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-04-16 21:26:23 -0700

    Add hh automine task

--- protocol/hardhat.config.ts
+++ protocol/hardhat.config.ts
@@ -1,11 +1,40 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomiclabs/hardhat-ethers";
 import "@nomicfoundation/hardhat-chai-matchers";
 import "hardhat-abi-exporter";
 import "@typechain/hardhat";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+task("automine", "Turn automine off or on")
+  .addFlag("off")
+  .addFlag("on")
+  .addOptionalParam("interval")
+  .setAction(
+    async (
+      args: { off: boolean; on: boolean; interval: string | undefined },
+      hre: HardhatRuntimeEnvironment
+    ) => {
+      if (args.on && args.off) {
+        throw "select only off or on.";
+      }
+      if (!args.on && !args.off) {
+        throw "select either --on or --off";
+      }
+
+      if (args.on) {
+        await hre.ethers.provider.send("evm_setAutomine", [true]);
+      } else {
+        await hre.ethers.provider.send("evm_setAutomine", [false]);
+      }
+      await hre.ethers.provider.send("evm_setIntervalMining", [
+        args.interval ? Number(args.interval) : 5000,
+      ]);
+    }
+  );
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  networks: {},
   abiExporter: [
     {
       runOnCompile: true,
