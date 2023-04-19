7eb8d12b9e5126844b33491d6bdfcb95802653a2
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-04-18 22:58:12 -0700

    Refactor exports

--- dapps/lib/index.ts
+++ dapps/lib/index.ts
@@ -2,11 +2,8 @@ import EthSymbol from "./components/EthSymbol.svelte";
 import ClockDisplay from "./components/ClockDisplay.svelte";
 import Web3Modal from "./components/Web3Modal.svelte";
 
-import { shorthandAddress, timeString, formatTime } from "./util";
+export * from "./util";
 
 export { EthSymbol };
 export { ClockDisplay };
 export { Web3Modal };
-export { shorthandAddress };
-export { timeString };
-export { formatTime };
