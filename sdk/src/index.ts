8aba2e18d366e712aa83ccabc4d53a91347f98c4
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-05-07 23:04:26 -0700

    Re arrange packages

---
+++ sdk/src/index.ts
@@ -0,0 +1,8 @@
+import { WalletClient } from "viem";
+import { ClientType, createClient } from "./chain-client";
+
+// Make the client, get commitment hub address, and create a signer
+export function createScpContext(signer: WalletClient): {
+
+} 
+}
