f0e6f0e19f4eef53bcdfeb0990992df93a97906e
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-05-10 23:17:53 -0700

    Add prettier stuff

--- dapps/social-alarm-clock/tailwind.config.js
+++ dapps/social-alarm-clock/tailwind.config.js
@@ -5,6 +5,9 @@ export default {
       fontFamily: {
         jura: ["Jura", "sans-serif"],
       },
+      backgroundColor: {
+        "transparent-grey": "rgba(30, 30, 35, 0.2)", // Customize the alpha value to set the transparency level you desire
+      },
     },
   },
   variants: {},
